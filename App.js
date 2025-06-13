205c10d5a99ecc0ecb19cb8ae71a0153ed4abb26
HITyaK <167464279+HITyaK@users.noreply.github.com>
2025-06-13 11:38:32 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,60 +1,12 @@
-/*import React from 'react';
-//import Inc from './Inc';
-import "./App.css";
-import ChildComponent from './ChildComponent';
-import ParentComponent from './ParentComponent';
-
-function App() {
-  return (
-  
-    <ParentComponent/>
-  
-  );
-
-}*/
-/*
 import React from 'react';
-import Weather from './Weather';
-
-function App(){
-  return(
-    <div>
-      <p> Welcom to weather website</p>
-      <Weather/>
-
-    </div>
-  );
-}
-
-
-export default App;
-*/
-
-import React, { useState } from 'react';
-import Counter from './Counter';
+import Login from './components/Login';
 
 function App() {
-  const [showCounter, setShowCounter] = useState(true);
-
-  const toggleCounter = () => {
-    setShowCounter((prevShow) => !prevShow);
-  };
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '30px' }}>
-      <button onClick={toggleCounter}>
-        {showCounter ? 'Hide' : 'Show'} Counter
-      </button>
-      {showCounter && <Counter />}
+    <div className="App">
+      <Login />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-   
-
